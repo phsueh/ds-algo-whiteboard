1df8c4df8a92c1f9acd949beea1a0d096d7ed13a
Paul Hsueh <hsueh.paul@gmail.com>
2021-10-30 13:13:58 -0400

    first question

---
+++ frequencyCounter-validAnagram.js
@@ -0,0 +1,3 @@
+// given two strings, write a function to determin is the same number of letters appear in both aka an Anagram. It's not like they have to be an actual work just that they have the same numbers 
+
+console.log("sup world")
