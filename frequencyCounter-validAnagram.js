cfe3c6ded61d4373285fc18f67f5e4680096e41d
Paul Hsueh <hsueh.paul@gmail.com>
2021-10-30 15:50:28 -0400

    anagram question

--- frequencyCounter-validAnagram.js
+++ frequencyCounter-validAnagram.js
@@ -1,3 +1,21 @@
-// given two strings, write a function to determin is the same number of letters appear in both aka an Anagram. It's not like they have to be an actual work just that they have the same numbers 
+// given two strings, write a function to determine if the strings have the same number of characters
 
-console.log("sup world")
+function validAnagram(str1, str2) {
+    if(str1.length != str2.length) {
+        return false
+    }
+
+    let anagramCompare1 = {}
+    let anagramCompare2 = {}
+
+    // 
+    for(let char in str1){
+        anagramCompare1[char] = (anagramCompare1[char] || 0) + 1
+        console.log(anagramCompare1)
+    }
+
+    return true
+
+}
+
+console.log(validAnagram("rat", "car"))
