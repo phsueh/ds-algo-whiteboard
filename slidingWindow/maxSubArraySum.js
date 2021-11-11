db60154454c0b25f6bdf33ccffbcb0ad54736f34
Paul Hsueh <hsueh.paul@gmail.com>
2021-11-11 15:57:44 -0500

    added coding exercise and solution

---
+++ slidingWindow/maxSubArraySum.js
@@ -0,0 +1,27 @@
+// Given an array of integers and a number, write a function, which finds the maximum sum of a subarray with the length of the nunber passed to the function
+
+function maxSubarraySum(arr, num){
+    if (arr.length < num) return null
+    
+    let maxSum = 0
+    let tempSum = 0
+    
+    for (let i = 0; i < num; i++){
+        maxSum += arr[i]
+    }
+    
+    tempSum = maxSum
+
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i-num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum) // reassigning value of maxSum 
+    }
+    return maxSum 
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2))
+
+// lessons learned 
+// "sliding window" does not mean that a window is moving through the loop rather. I am just subtracting and adding 
+// The loop keeps running until we get through every value and then returns the maxSum
+    // maxSum gets reassigned 
---
+++ slidingWindow/minSubArrayLen.js
@@ -0,0 +1,7 @@
+// write a function that accepts 2 parameters - an array of possitive integers and a positive integer
+
+// function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one return 0
+
+function minSubArrayLen(){
+    
+}
