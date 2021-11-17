b816642534a8881e0935b6282bf555995ee30632
Paul Hsueh <hsueh.paul@gmail.com>
2021-11-17 18:50:20 -0500

    added params

--- slidingWindow/minSubArrayLen.js
+++ slidingWindow/minSubArrayLen.js
@@ -2,6 +2,6 @@
 
 // function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one return 0
 
-function minSubArrayLen(){
+function minSubArrayLen(arr, sum){
     
 }
