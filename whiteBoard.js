7331115c0f552ea773b5e78125e70468b4d742c0
Paul Hsueh <hsueh.paul@gmail.com>
2023-11-30 22:29:20 -0600

    more practice

--- whiteBoard.js
+++ whiteBoard.js
@@ -1,24 +1,27 @@
-function countOccurrences(arr) {
-  // Initialize an array to store the counts with a fixed size of 100
-  const counts = Array(100).fill(0);
-
-  // Count occurrences of each value
-  arr.forEach(value => {
-    counts[value]++;
-  });
-
-  // Generate the result array based on counts
+function groupThePeople(groupSizes) {
   const result = [];
-  for (let i = 0; i < counts.length; i++) {
-    for (let count = 0; count < counts[i]; count++) {
-      result.push(i);
-    }
+  const groups = {};
+
+  for (let i = 0; i < groupSizes.length; i++) {
+      const size = groupSizes[i];
+
+      if (!(size in groups)) {
+          groups[size] = [i]; // Initialize group for the given size
+      } else {
+          groups[size].push(i); // Add person to the corresponding group
+      }
+
+      // If the group is complete, add it to the result and reset the group
+      if (groups[size].length === size) {
+          result.push(groups[size]);
+          groups[size] = [];
+      }
   }
 
   return result;
 }
 
 // Example usage:
-const inputArray = [4, 2, 4, 5, 2, 3, 1, 3];
-const resultArray = countOccurrences(inputArray);
-console.log(resultArray);
+const groupSizes = [3, 3, 3, 3, 3, 1, 3];
+const result = groupThePeople(groupSizes);
+console.log(result);
