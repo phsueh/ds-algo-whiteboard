fb184e74fcfe4566fa93887ae3fba45c018429d0
Paul Hsueh <hsueh.paul@gmail.com>
2023-12-03 16:00:04 -0600

    linked list practice

--- whiteBoard.js
+++ whiteBoard.js
@@ -1,27 +1,30 @@
-function groupThePeople(groupSizes) {
-  const result = [];
-  const groups = {};
-
-  for (let i = 0; i < groupSizes.length; i++) {
-      const size = groupSizes[i];
-
-      if (!(size in groups)) {
-          groups[size] = [i]; // Initialize group for the given size
-      } else {
-          groups[size].push(i); // Add person to the corresponding group
-      }
-
-      // If the group is complete, add it to the result and reset the group
-      if (groups[size].length === size) {
-          result.push(groups[size]);
-          groups[size] = [];
-      }
+class Node { 
+  constructor(val) {
+    this.value = val;
+    this.next  = null;
   }
-
-  return result;
 }
 
-// Example usage:
-const groupSizes = [3, 3, 3, 3, 3, 1, 3];
-const result = groupThePeople(groupSizes);
-console.log(result);
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null; 
+    this.length = 0;
+ }
+
+// head is to the left think of it as a caterlpiller
+// we push to the end of the list
+
+ push(val) { // head and tail all reference a node
+  let newNode = new Node(val);
+  if (!this.head) {
+    this.head = newNode;
+    this.tail = this.head;
+  } else {
+    this.tail.next = newNode; 
+    this.tail = newNode; 
+  }
+  this.length++;
+  return this; // this linked list
+ }
+}
