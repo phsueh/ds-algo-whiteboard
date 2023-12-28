741950e3cc6b641c68a8c3b5097f542cb722efda
Paul Hsueh <hsueh.paul@gmail.com>
2023-12-27 21:10:22 -0600

    more practice

--- whiteBoard.js
+++ whiteBoard.js
@@ -1,26 +1,49 @@
-let selectionSort = (arr) => {
+function solution(src) {
 
-    // example = [3,0,4,2]
+    let srcArr = src.split(" ");
+    let resultArr = new Array; 
 
-    for (let i = 0; i < arr.length; i++) {
-        let lowest = i;
-        for (let j = 1+i; j < arr.length; j++) {
-            if (arr[j] < arr[lowest]) {
-                lowest = j; 
+    console.log(src)
+
+    for (let i = 0; i < srcArr.length; i++) {
+        let str = srcArr[i];
+        let tempStr = "";
+        for (let j = 0; j < str.length; j++) {
+            if (str[j] === '_' && str[j+1] !== '_' && str[j-1] !== '_' && str[j+1] !== undefined && str[j-1] !== undefined) {
+                tempStr += str[j+1].toUpperCase(); 
+                j += 2;
             }
+            tempStr += str[j];
         }
-        if (i !== lowest) {
-            let temp = arr[i];
-            arr[i] = arr[lowest]
-            arr[lowest] = temp; 
-        }
+        resultArr.push(tempStr)
     }
 
-    return arr;
+    return resultArr.join(" ");
 
 }
 
+let solution2 = function(src) {
+    console.log(src)
+    let resultStr = "";
+    for (let i = 0; i < src.length; i++) {
+        if (
+            src.charCodeAt(i) === 95 && 
+            src.charCodeAt(i+1) !== 95 && 
+            src.charCodeAt(i-1) !== 95 && 
+            src.charCodeAt(i+1) !== 32 &&
+            src.charCodeAt(i-1) !== 32) {
+                resultStr += src[i+1].toUpperCase();
+                i += 2; 
+        }
+        resultStr += src[i];
+    }
+    return resultStr; 
+}
+
 console.log(
-  selectionSort([0,2,34,22,10,19,17]), 
-  selectionSort([-3, -2, -1, 0, 1, 2, 3, 34])
+    solution("the comp_anies co_vention is to use __camel_case not_ __sn_ake case like _the_variable"),
+)
+
+console.log(
+    solution2("the comp_anies co_vention is to use __camel_case not_ __sn_ake case like _the_variable")
 )
