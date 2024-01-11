45aab4d4d00ac69ed59e8100f32bb948b46e463a
Paul Hsueh <hsueh.paul@gmail.com>
2024-01-11 11:41:54 -0500

    more practice

--- whiteBoard.js
+++ whiteBoard.js
@@ -1,33 +1,40 @@
-var majorityElement = function(nums) {
-    
-    // hash table
-    // find largest value and return the key
+// sort - sort both 2 arrays
+// merge sort - split the array down to smaller array sand recursively call sort and the left and right of arr
 
-    let hashTable = {}; 
+function sort(arr1, arr2) {
+    let result = []; 
+    let i = 0; 
+    let j = 0; 
 
-    for (let i = 0; i < nums.length; i++) {
-        if (hashTable[nums[i]] > 0) {
-            hashTable[nums[i]]++;
+    while(i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i])
+            i++;
         } else {
-            hashTable[nums[i]] = 1; 
+            result.push(arr2[j])
+            j++;
         }
     }
 
-    let count = 0;
-    let majEle; 
-
-    for (let ele in hashTable) {
-        if (hashTable[ele] > count) {
-            count = hashTable[ele];
-            majEle = ele; 
-        }
+    while(i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
     }
 
-    return majEle;
+    while(j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
 
-};
+    return result;
+}
 
-console.log(
-    majorityElement([3,2,3]),
-    // majorityElement([2,2,1,1,1,2,2])
-)
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr; 
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return sort(left, right);
+}
+
+console.log(mergeSort([8,1,2,3,4,5,6,7]));
